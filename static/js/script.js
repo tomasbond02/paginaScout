6713534f84bcf58b2ff8427242dda786bb2bf3d3
tomasbond02 <bondtomy@gmial.com>
2022-06-15 07:50:44 -0300

    main

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,35 @@
 const baseDeDatos = [
     {
         id: 1,
-        nombre: '',
-        description: '',
+        nombre: 'camisa',
+        descripcion: 'esto es una camisa',
         precio: 0,
         imagen: '',
-        categoria: ''
+        categoria: 'puto'
+    },
+    {
+        id: 2,
+        nombre: 'camisa',
+        descripcion: 'esto es una camisa',
+        precio: 150,
+        imagen: '',
+        categoria: 'perro'
+    },
+    {
+        id: 3,
+        nombre: 'camisa',
+        descripcion: 'esto es una camisa',
+        precio: 200,
+        imagen: '',
+        categoria: 'puto'
+    },
+    {
+        id: 4,
+        nombre: 'camisa',
+        descripcion: 'esto es una camisa',
+        precio: 4000,
+        imagen: '',
+        categoria: 'perro'
     }
 
 ];
@@ -13,23 +37,53 @@ const baseDeDatos = [
 
 
 let carrito = [];
-let botonCarrito = document.getElementById("boton");
-let mostrando = document.getElementById("contenedorP");
-let mostrarTexto = document.getElementById("texto");
-let mensaje ="";
-function prueba(){
-    
-    if (mostrando.style.backgroundColor == "red"){
-        mostrarTexto.style.fontSize = "150px";
-    }
-    else if (mostrando.style.backgroundColor == "blue") {
-        mensaje.innerHTML="<p>hola puta</p>";
-    }
-    
+let listadoDeProductos = document.getElementById("listadoDeProductos")
+
+function agregarProductos(id){
+    alert(id)
 }
 
-botonCarrito.addEventListener("click",prueba);
+for(let i of baseDeDatos){
+    let contenedorProducto = document.createElement("div")
+    contenedorProducto.style.borderStyle = 'dashed'
+    contenedorProducto.style.margin = '2px'
+    contenedorProducto.style.width = '300px'
+    contenedorProducto.style.display = 'flex'
+    contenedorProducto.style.flexDirection = 'column'
+    contenedorProducto.style.justifyContent = 'center'
 
-function añadirCarrito(index){
-    carrito.push(Object.assign(baseDeDatos[index]));
+    let id = document.createElement("p")
+    id.innerText = i.id
+    id.className = 'ml-2'
+
+    let nombre = document.createElement('p')
+    nombre.innerText = i.nombre
+    nombre.className = 'ml-2'
+
+    let descripcion = document.createElement('p')
+    descripcion.innerText = i.descripcion
+    descripcion.className = 'ml-2'
+
+    let categoria = document.createElement('p')
+    descripcion.innerText = i.categoria
+    descripcion.className = 'ml-2'
+
+    let precio = document.createElement('p')
+    descripcion.innerText = i.precio
+    descripcion.className = 'ml-2'
+
+    let button = document.createElement("button")
+    button.innerHTML = 'agregar'
+    button.onclick = function(){
+        agregarProductos(i.id)
+    }
+
+    contenedorProducto.appendChild(id)
+    contenedorProducto.appendChild(nombre)
+    contenedorProducto.appendChild(descripcion)
+    contenedorProducto.appendChild(precio)
+    contenedorProducto.appendChild(categoria)
+    contenedorProducto.appendChild(button)
+
+    listadoDeProductos.appendChild(contenedorProducto)
 }
