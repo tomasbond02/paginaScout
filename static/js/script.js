53cc917aa934c2cc4c98fd8d23a0f62e81593947
tomasbond02 <bondtomy@gmial.com>
2022-06-15 08:25:20 -0300

    agrego carrito

--- static/js/script.js
+++ static/js/script.js
@@ -1,89 +1,150 @@
-const baseDeDatos = [
+products = [
     {
-        id: 1,
-        nombre: 'camisa',
-        descripcion: 'esto es una camisa',
-        precio: 0,
-        imagen: '',
-        categoria: 'puto'
+        id:1,
+        name:"Gorra",
+        description:"esto es una gorra",
+        price:20,
+        image: ""
     },
     {
-        id: 2,
-        nombre: 'camisa',
-        descripcion: 'esto es una camisa',
-        precio: 150,
-        imagen: '',
-        categoria: 'perro'
+        id:2,
+        name:"Banana",
+        description:"esto es una banana",
+        price:30,
+        image: ""
     },
     {
-        id: 3,
-        nombre: 'camisa',
-        descripcion: 'esto es una camisa',
-        precio: 200,
-        imagen: '',
-        categoria: 'puto'
-    },
-    {
-        id: 4,
-        nombre: 'camisa',
-        descripcion: 'esto es una camisa',
-        precio: 4000,
-        imagen: '',
-        categoria: 'perro'
+        id:3,
+        name:"Teclado",
+        description:"esto es un teclado",
+        price:40,
+        image: ""
     }
-
-];
-
-
-
-let carrito = [];
-let listadoDeProductos = document.getElementById("listadoDeProductos")
-
-function agregarProductos(id){
-    alert(id)
-}
-
-for(let i of baseDeDatos){
-    let contenedorProducto = document.createElement("div")
-    contenedorProducto.style.borderStyle = 'dashed'
-    contenedorProducto.style.margin = '2px'
-    contenedorProducto.style.width = '300px'
-    contenedorProducto.style.display = 'flex'
-    contenedorProducto.style.flexDirection = 'column'
-    contenedorProducto.style.justifyContent = 'center'
-
-    let id = document.createElement("p")
-    id.innerText = i.id
-    id.className = 'ml-2'
-
-    let nombre = document.createElement('p')
-    nombre.innerText = i.nombre
-    nombre.className = 'ml-2'
-
-    let descripcion = document.createElement('p')
-    descripcion.innerText = i.descripcion
-    descripcion.className = 'ml-2'
-
-    let categoria = document.createElement('p')
-    descripcion.innerText = i.categoria
-    descripcion.className = 'ml-2'
-
-    let precio = document.createElement('p')
-    descripcion.innerText = i.precio
-    descripcion.className = 'ml-2'
-
-    let button = document.createElement("button")
-    button.innerHTML = 'agregar'
-    button.onclick = function(){
-        agregarProductos(i.id)
+    
+  ]
+  carrito = []
+  
+  function agregarACarrito(index){
+     
+    let productExist = false;
+    carrito.forEach(element => {
+         
+        if(element.product.id == (index + 1)){
+            element.count = element.count + 1
+            productExist = true; 
+        }
+    })
+    if(!productExist){
+        carrito.push({
+            product:Object.assign(products[index]),
+            count:1
+        })
     }
-
-    contenedorProducto.appendChild(id)
-    contenedorProducto.appendChild(nombre)
-    contenedorProducto.appendChild(descripcion)
-    contenedorProducto.appendChild(precio)
-    contenedorProducto.appendChild(categoria)
-    contenedorProducto.appendChild(button)
-
-    listadoDeProductos.appendChild(contenedorProducto)
-}
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    renderizarProductosCarrito();
+  }
+  
+  function quitarDelCarrito(index){
+    carrito.splice(index,1);
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    renderizarProductosCarrito();
+  }
+  
+  
+  function renderizarProductos(){
+    let nodoPrincipal = document.getElementById("products")
+    if(nodoPrincipal==null){
+      return
+    }
+  
+    products.forEach(product => {
+        let nodoProducto = document.createElement("div")
+        nodoProducto.className = " m-2 border border-primary text-center"
+  
+        const title = document.createElement("h5")
+        title.innerText = product.name
+  
+        const description = document.createElement("p")
+        description.innerText = product.description
+  
+        const price = document.createElement("p")
+        price.innerText = `$ ${product.price.toString()}`
+  
+        let button = document.createElement("button")
+        button.onclick = function(){ agregarACarrito(product.id - 1)  }
+        button.innerText = "Agregar al Carrito"
+        button.className = "btn btn-primary m-1"
+  
+        nodoProducto.appendChild(title)
+        nodoProducto.appendChild(description)
+        nodoProducto.appendChild(price)
+        nodoProducto.appendChild(button)
+  
+        nodoPrincipal.appendChild(nodoProducto)
+    });
+  }
+  
+  function renderizarProductosCarrito(){
+    let priceTotal = 0
+    let priceCart = document.getElementById("priceCart")
+  
+    let carritoRecuperado = JSON.parse( localStorage.getItem('carrito') );
+    if(carritoRecuperado){
+      carrito = carritoRecuperado;
+    }
+  
+    contarProductos()
+  
+    let nodoPrincipal = document.getElementById("carrito")
+    if(nodoPrincipal==null){
+      return
+    }
+  
+    nodoPrincipal.innerHTML = ""
+    for (let i in carrito) {
+        let element = carrito[i]
+        priceTotal = priceTotal + (element.product.price * element.count)
+  
+        let nodoProducto = document.createElement("div")
+        nodoProducto.className = " m-2 border border-primary text-center"
+  
+  
+        const title = document.createElement("h5")
+        title.innerText = element.product.name
+  
+        const priceAndCount = document.createElement("p")
+        priceAndCount.innerText =  `Cantidad: ${element.count.toString()} | Precio: ${element.product.price.toString()} `
+        priceAndCount.className = "m-1"
+  
+        const button = document.createElement("button")
+        button.onclick = function(){ quitarDelCarrito(i)  }
+        button.innerText = "Quitar"
+        button.className = "btn btn-danger m-1"
+  
+  
+        nodoProducto.appendChild(title)
+        nodoProducto.appendChild(priceAndCount)
+        nodoProducto.appendChild(button)
+  
+        nodoPrincipal.appendChild(nodoProducto)
+    };
+  
+    priceCart.innerText = `Total del carrito $ ${priceTotal.toString()}`
+  }
+  
+  function contarProductos() {
+    let count = 0
+    carrito.forEach(element => {
+      count = count + element.count
+    });
+    let notificationDivCount = document.getElementById("notificationDivCount")
+    if(notificationDivCount!=null & count!=0){
+      notificationDivCount.innerHTML = notificationDivCount.innerHTML + `<span class="badge">${count}</span>`
+    }
+  }
+  
+  setTimeout(() => {
+    renderizarProductos()
+    renderizarProductosCarrito()
+    contarProductos()
+  }, 200);
